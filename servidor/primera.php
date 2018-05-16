87836342e18d97a3d262348b633ca7c3a0a19c04
CCDM12 <hallo_pedro10@hotmail.com>
2018-05-16 11:53:41 -0600

    Primer PHP

---
+++ servidor/primera.php
@@ -0,0 +1,21 @@
+<?php
+	$parametro1 = $_GET["parametro1"];
+	$variable = 0;
+	$otra = "";
+	$otradeotra = 0.0;
+
+	for ($i=0; $i <= 10 ; $i++) 
+	{ 
+		print("Texto " .$i. "<br>");
+	}
+
+?>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	Nitzia Marquez <?php print($parametro1); ?>
+</body>
+</html>
+
